620e490fe923f9643102d68a5a6e7d78dcec32b7
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2017-05-30 19:33:42 -0500

    Clase 4 En esta clase se especifican dos tipos de datos Floants y Boleanos tamabien la funcion de los diferentes tipos de operadores de este lenguaje

---
+++ clase4/operadores.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  a := 5
+  b := 3
+  c := 9
+  //operadores aritmeticos
+  fmt.Println(a+b)
+  fmt.Println(a-b)
+  fmt.Println(a*b)
+  fmt.Println(a/b)
+  fmt.Println(a%b)
+
+  fmt.Println("*******************")
+  //
+  // //operacion de comparacion
+  //
+  fmt.Println(b == c)
+  fmt.Println(b != c)
+  fmt.Println(a < b)
+  fmt.Println(a > c)
+  fmt.Println(a >= b)
+  fmt.Println(a <= b)
+  //
+  // 
+  //
+  // //operadores de asignacion
+  //
+  // += --> a +=b =>  a = a + b
+  // -= --> a -=b =>  a = a - b
+  // *= --> a *=b =>  a = a * b
+  // /= --> a /=b =>  a = a / b
+  // %= --> a %=b =>  a = a % b
+
+
+}
